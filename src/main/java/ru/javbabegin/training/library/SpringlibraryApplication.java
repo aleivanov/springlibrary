733319e7b62551b30d787a21d80935571ae7e615
aleivanov <kviner@narod.ru>
2017-10-04 00:24:18 +0300

    start

---
+++ src/main/java/ru/javbabegin/training/library/SpringlibraryApplication.java
@@ -0,0 +1,12 @@
+package ru.javbabegin.training.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringlibraryApplication.class, args);
+	}
+}
